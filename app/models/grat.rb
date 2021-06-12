f31212252d452d621819b412949dd77c7fa9c08f
andydenenberg <andydenenberg@gmail.com>
2021-06-12 14:53:43 -0500

    structure history

--- app/models/grat.rb
+++ app/models/grat.rb
@@ -52,8 +52,8 @@ class Grat < ApplicationRecord
     end
 
     text += "<br>"
-    text += "Residual Total Gain = #{ActiveSupport::NumberHelper.number_with_delimiter(residual_total_gain)}<br>"
-    text += "Daily Total = #{ActiveSupport::NumberHelper.number_with_delimiter(daily_total_change)}<br>"
+    text += "Residual Total Gain = #{ActiveSupport::NumberHelper.number_to_delimited(residual_total_gain)}<br>"
+    text += "Daily Total = #{ActiveSupport::NumberHelper.number_to_delimited(daily_total_change)}<br>"
     text += "<br>"
     
     return [ text, history, residual_total_gain.to_s, daily_total_change.to_s ]
