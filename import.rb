3308e331844558c207c3bbeaa8e58760ec90699a
andydenenberg <andydenenberg@gmail.com>
2021-06-18 08:15:41 -0500

    add redis in controllers

---
+++ import.rb
@@ -0,0 +1,10 @@
+  require 'csv'
+     file = File.open("/Users/andydenenberg/Downloads/quotes.csv")
+     data = CSV.parse(file.read)
+     puts
+     puts "Stock.delete_all"
+     puts
+     data[1..data.length].each do |s|
+     	lc = s[4] ? s[4] : 0
+     	puts "Stock.create symbol: '#{s[0]}', last_price: #{s[1]}, last_change: #{lc} "
+     end 
