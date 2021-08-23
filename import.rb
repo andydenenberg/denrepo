fee8b0abbf7099034288a48e7be0536ba208b7d1
andydenenberg <andydenenberg@gmail.com>
2021-08-23 16:41:58 -0500

    increase the y axis

--- import.rb
+++ import.rb
@@ -8,3 +8,4 @@
      	lc = s[4] ? s[4] : 0
      	puts "Stock.create symbol: '#{s[0]}', last_price: #{s[1]}, last_change: #{lc} "
      end 
+File.delete('/Users/andydenenberg/Downloads/quotes.csv') if File.exist?('/Users/andydenenberg/Downloads/quotes.csv')
